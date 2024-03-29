0afb361b556b8fe90f588417e0eb1386ec458600
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-03-29 11:15:56 +0100

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import {View} from "react-native"
+
+export default function App() {
+  return (
+    <View style={{
+      backgroundColor: '#fff',
+      flex: 1,
+    }}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        flex: 2,
+      }}/>
+      <View style={{
+        backgroundColor: 'gold',
+        flex: 1,
+      }}/>
+      <View style={{
+        backgroundColor: 'tomato',
+        flex: 1,
+      }}/>
+    </View>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Application de Demo pour React Native
+
+Cette application permet de voir un exemple d'application mobile utilisant React Native.
